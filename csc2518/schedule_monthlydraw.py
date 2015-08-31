5ab120f5cc4a66f28370c7bd8f5e3772bcd17f64
Maria Yancheva <maria.yancheva@gmail.com>
2015-08-31 12:56:00 -0400

    Added script that runs the monthly prize draw

---
+++ csc2518/schedule_monthlydraw.py
@@ -0,0 +1,27 @@
+import datetime
+import os
+import requests
+
+from settings import SYSTEM_CRED
+
+def generate_log(r):
+    return "\n".join([ "HTTP Response Status: %d (%s)" % (r.status_code, r.reason), \
+                       "HTTP Response JSON: ", \
+                       "\n".join([k + "=" + v for (k,v) in r.json().iteritems()]) \
+                       ])
+
+    
+# Send out the reminders, and log the HTTP response from the server
+r = requests.post("https://www.cs.toronto.edu/talk2me/maintenance/mailer/monthlydraw", \
+                  data={'auth_name': 'system', \
+                        'auth_pass': SYSTEM_CRED})
+                        
+# Log the result
+date_format = '%Y%m%d_%H%M%S'
+timestamp = datetime.datetime.now().strftime(date_format)
+base_dir = os.sep.join(['..', '..', 'csc2518_logs', 'monthlydraw'])
+log_file = os.path.join(base_dir, timestamp + '.log')
+
+with open(log_file, 'w') as fout:
+    fout.write(generate_log(r))
+
