1d1bc1b45fcd7979c747efa41a13ae9e09a12d7b
Maria Yancheva <maria.yancheva@gmail.com>
2015-09-01 18:01:17 -0400

    Added a view and template for the Admin UI (dashboard) page

---
+++ datacollector/adminui.py
@@ -0,0 +1,51 @@
+from django.db.models import Q, Count
+from django.contrib.auth import authenticate, login as auth_login, logout as auth_logout
+from django.http import HttpResponseRedirect, HttpResponse
+from django.shortcuts import render_to_response, get_object_or_404
+from django.template import RequestContext
+from django.utils import simplejson
+from datacollector.models import *
+from csc2518.settings import STATIC_URL
+from csc2518.settings import SUBSITE_ID
+
+import notify
+
+
+# Set up mail authentication
+global email_username, email_name, website_hostname
+email_username = Settings.objects.get(setting_name="system_email").setting_value
+email_name = Settings.objects.get(setting_name="system_email_name").setting_value
+website_hostname = Settings.objects.get(setting_name="website_hostname").setting_value
+website_name = Settings.objects.get(setting_name="website_name").setting_value
+
+# Globals
+global global_passed_vars, date_format, age_limit, regex_email, regex_date, colour_lookup
+global_passed_vars = { "website_id": "talk2me", "website_name": website_name, "website_email": email_username }
+website_root = '/'
+if SUBSITE_ID: website_root += SUBSITE_ID
+
+
+def dashboard(request):
+    is_authenticated = False
+    consent_submitted = None
+    demographic_submitted = None
+    active_notifications = None
+    
+    if request.user.is_authenticated() and request.user.is_superuser:
+        is_authenticated = True
+        subject = Subject.objects.filter(user_id=request.user.id)
+        if subject:
+            subject = subject[0]
+            consent_submitted = subject.date_consent_submitted
+            demographic_submitted = subject.date_demographics_submitted
+            
+            # Fetch all notifications that are active and have not been dismissed by the user 
+            # (NB: Q objects must appear before keyword parameters in the filter)
+            active_notifications = notify.get_active_new(subject)
+        
+        passed_vars = {'is_authenticated': is_authenticated, 'consent_submitted': consent_submitted, 'demographic_submitted': demographic_submitted, 'active_notifications': active_notifications, 'user': request.user }
+        passed_vars.update(global_passed_vars)
+        return render_to_response('datacollector/adminui.html', passed_vars, context_instance=RequestContext(request))
+    else:
+        return HttpResponseRedirect(website_root)
+    
