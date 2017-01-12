6de3012687f81ed4a29fb2e828ba32a017298d3c
Maria Yancheva <maria.yancheva@gmail.com>
2017-01-12 12:44:55 -0500

    Added all API endpoints in the URLs spec

--- csc2518/urls.py
+++ csc2518/urls.py
@@ -43,9 +43,12 @@ urlpatterns = patterns('',
 	url(r'^' + settings.SUBSITE_ID + 'phone/status$', 'datacollector.phone.status'),
     url(r'^' + settings.SUBSITE_ID + 'phone/login$', 'datacollector.phone.login'),
     url(r'^' + settings.SUBSITE_ID + 'phone/session$', 'datacollector.phone.session'),
-    url(r'^' + settings.SUBSITE_ID + 'phone/session/(?P<session_id>[0-9]+)$', 'datacollector.phone.session_tasklist'),
+    url(r'^' + settings.SUBSITE_ID + 'phone/session/(?P<session_id>[0-9]+)$', 'datacollector.phone.session_id'),
     url(r'^' + settings.SUBSITE_ID + 'phone/session_task/(?P<session_task_id>[0-9]+)$', 'datacollector.phone.session_task'),
     url(r'^' + settings.SUBSITE_ID + 'phone/response$', 'datacollector.phone.response'),
+    url(r'^' + settings.SUBSITE_ID + 'phone/task$', 'datacollector.phone.task'),
+    url(r'^' + settings.SUBSITE_ID + 'phone/task_value$', 'datacollector.phone.task_value'),
+    url(r'^' + settings.SUBSITE_ID + 'phone/difficulty_level$', 'datacollector.phone.difficulty_level'),
     
     # Notifications
     url(r'^' + settings.SUBSITE_ID + 'notify/dismiss/$', 'datacollector.notify.dismiss'),
