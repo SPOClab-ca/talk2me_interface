18e8e3bb3645de001a46d4cd66fd4fe0a52ae0cb
Chloe Pou-Prom <chloepouprom@gmail.com>
2017-06-30 18:40:23 -0400

    Shuffle task instances when creating a new UHN session

--- datacollector/tests/test_views.py
+++ datacollector/tests/test_views.py
@@ -6,8 +6,10 @@ from django.contrib.auth.models import User
 from django.test import TestCase
 from django.test import Client
 
-from datacollector.models import (Bundle, Subject, Subject_Bundle, Language, Session,
-                                  Session_Type, Session_Task, Task, Bundle_Task)
+from datacollector.models import (Bundle, Bundle_Task_Field_Value, Field_Data_Type, Field_Type, Subject,
+                                  Subject_Bundle, Language, Session, Session_Type, Session_Task, Session_Task_Instance,
+                                  Session_Task_Instance_Value, Task, Task_Field, Task_Field_Value, Value_Difficulty,
+                                  Bundle_Task)
 from datacollector import views
 
 from utils import (create_web_user, create_uhn_web_bundle, create_uhn_phone_bundle,
@@ -96,10 +98,10 @@ class ViewsGenerateSessionTestCase(TestCase):
         session_type_phone = Session_Type.objects.create(session_type_id=2, name='phone', text_only=1)
 
         # Populate the DB with a few tasks
-        bundle_task = Task.objects.create(task_id=1, name_id='task1', name='task1',
-                                          instruction='instruction', default_order=1,
-                                          is_order_fixed=0, default_delay=0,
-                                          default_embedded_delay=0, is_active=1)
+        task_no_repeat = Task.objects.create(task_id=1, name_id='task1', name='task1',
+                                             instruction='instruction', default_order=1,
+                                             is_order_fixed=1, default_delay=0,
+                                             default_embedded_delay=0, is_active=1)
         active_task = Task.objects.create(task_id=2, name_id='task2', name='task2',
                                           instruction='inst', default_order=100,
                                           is_order_fixed=0, default_delay=0,
@@ -108,14 +110,49 @@ class ViewsGenerateSessionTestCase(TestCase):
                                             instruction='instructionnn', default_order=20,
                                             is_order_fixed=1, default_delay=0,
                                             default_embedded_delay=0, is_active=0)
+        task_repeat = Task.objects.create(task_id=4, name_id='bundle_task_allow_repeat',
+                                          name='bundle_task_allow_repeat', instruction='',
+                                          default_order=100, is_order_fixed=0, default_delay=0,
+                                          default_embedded_delay=0, is_active=1)
+
+        # Insert dummy field type, field data type, and value difficulty
+        field_type = Field_Type.objects.create(field_type_id=1, name='display')
+        field_data_type = Field_Data_Type.objects.create(field_data_type_id=1, name='text')
+        value_difficulty = Value_Difficulty.objects.create(value_difficulty_id=1, name='low')
+
+        # Create task fields for tasks
+        task_field_bundle_no_repeat = Task_Field.objects.create(task_field_id=1, name='',
+                                                                task_id=task_no_repeat.task_id,
+                                                                field_type_id=field_type.field_type_id,
+                                                                field_data_type_id=field_data_type.field_data_type_id,
+                                                                embedded_response=0, generate_value=1)
+        task_field_bundle_repeat = Task_Field.objects.create(task_field_id=2, name='',
+                                                             task_id=task_repeat.task_id,
+                                                             field_type_id=field_type.field_type_id,
+                                                             field_data_type_id=field_data_type.field_data_type_id,
+                                                             embedded_response=0, generate_value=1)
+
+        # Add task field values for the task fields
+        task_field_value1 = Task_Field_Value.objects.create(task_field_value_id=1,
+                                                            task_field_id=task_field_bundle_no_repeat.task_field_id,
+                                                            value='task field value should not repeat',
+                                                            difficulty_id=value_difficulty.value_difficulty_id)
+        task_field_value2 = Task_Field_Value.objects.create(task_field_value_id=2,
+                                                            task_field_id=task_field_bundle_no_repeat.task_field_id,
+                                                            value='task field value should be different',
+                                                            difficulty_id=value_difficulty.value_difficulty_id)
+        task_field_value3 = Task_Field_Value.objects.create(task_field_value_id=3,
+                                                            task_field_id=task_field_bundle_repeat.task_field_id,
+                                                            value='this task field can have repeat instances',
+                                                            difficulty_id=value_difficulty.value_difficulty_id)
 
         # Create a UHN user
         user_uhn = User.objects.create_user(username='user_uhn', email='@', password='user_uhn')
         subject_uhn_bundle = Subject.objects.create(user_id=user_uhn.id, date_created=TODAY,
                                                     date_consent_submitted=TODAY,
                                                     date_demographics_submitted=TODAY)
-        Bundle.objects.create(bundle_id=1, name_id='uhn_web', bundle_token='123qwert')
-        Subject_Bundle.objects.create(subject_bundle_id=1, subject_id=subject_uhn_bundle.user_id, bundle_id=1,
+        uhn_web_bundle = Bundle.objects.create(bundle_id=3, name_id='uhn_web', bundle_token='123qwert')
+        Subject_Bundle.objects.create(subject_bundle_id=1, subject_id=subject_uhn_bundle.user_id, bundle_id=uhn_web_bundle.bundle_id,
                                       active_startdate=TODAY)
 
         # Create a web user
@@ -124,27 +161,51 @@ class ViewsGenerateSessionTestCase(TestCase):
                                              date_consent_submitted=TODAY,
                                              date_demographics_submitted=TODAY)
 
-        # Assign task to bundle
-        Bundle_Task.objects.create(bundle_task_id=1, bundle_id=1, task_id=bundle_task.task_id, default_num_instances=4)
+        # Assign task to bundle (this task cannot have repeat instances)
+        bundle_task_no_repeat = Bundle_Task.objects.create(bundle_task_id=views.RIG_UHN_WEB_BUNDLE_TASK_ID,
+                                                           bundle_id=uhn_web_bundle.bundle_id,
+                                                           task_id=task_no_repeat.task_id,
+                                                           default_num_instances=1)
+
+        # Assign task to bundle (this task can have repeat instances)
+        bundle_task_repeat = Bundle_Task.objects.create(bundle_task_id=1,
+                                                        bundle_id=uhn_web_bundle.bundle_id,
+                                                        task_id=task_repeat.task_id,
+                                                        default_num_instances=1)
+
+
+        # Assign task instances to bundle
+        Bundle_Task_Field_Value.objects.create(bundle_task_field_value_id=1,
+                                               bundle_task_id=bundle_task_no_repeat.bundle_task_id,
+                                               task_field_value_id=task_field_value1.task_field_value_id)
+        Bundle_Task_Field_Value.objects.create(bundle_task_field_value_id=2,
+                                               bundle_task_id=bundle_task_no_repeat.bundle_task_id,
+                                               task_field_value_id=task_field_value2.task_field_value_id)
+        Bundle_Task_Field_Value.objects.create(bundle_task_field_value_id=3,
+                                               bundle_task_id=bundle_task_repeat.bundle_task_id,
+                                               task_field_value_id=task_field_value3.task_field_value_id)
 
         # Save on the object to re-use later
         self.subject_uhn_bundle = subject_uhn_bundle
         self.subject_web = subject_web
         self.session_type_web = session_type_web
         self.session_type_phone = session_type_phone
-        self.bundle_task = bundle_task
+        self.bundle_task = bundle_task_no_repeat
         self.active_task = active_task
         self.inactive_task = inactive_task
+        self.task_no_repeat = task_no_repeat
+        self.task_repeat = task_repeat
 
     def test_generate_session_uhn(self):
         '''
-            test_generate_session_uhn should only return tasks in UHN bundle
+            test_generate_session_uhn should only return tasks in UHN bundle and respect
+            their defined order
         '''
 
         uhn_session = views.generate_session(self.subject_uhn_bundle, self.session_type_web)
-        session_tasks = Session_Task.objects.filter(session=uhn_session)
-
+        session_tasks = Session_Task.objects.filter(session=uhn_session).order_by('order')
         self.assertEquals(session_tasks[0].task_id, self.bundle_task.task_id)
+        self.assertEquals(session_tasks[1].task_id, self.task_repeat.task_id)
 
     def test_generate_session_web_user(self):
         '''
@@ -153,9 +214,32 @@ class ViewsGenerateSessionTestCase(TestCase):
 
         web_session = views.generate_session(self.subject_web, self.session_type_web)
         session_tasks = Session_Task.objects.filter(session=web_session)
-        self.assertEquals(len(session_tasks), 2)
-        self.assertEquals(session_tasks[0].task_id, self.bundle_task.task_id)
-        self.assertEquals(session_tasks[1].task_id, self.active_task.task_id)
+        self.assertEquals(len(session_tasks), 3)
+
+    def test_generate_session_uhn_multiple_sessions(self):
+        '''
+            test_generate_session_uhn_multiple_sessions should populate the session with
+            different task instances if specified.
+        '''
+
+        first_session = views.generate_session(self.subject_uhn_bundle, self.session_type_web)
+        first_session_task_no_repeat = Session_Task.objects.get(session_id=first_session.session_id, task_id=self.task_repeat.task_id)
+        first_session_task_no_repeat_instance = Session_Task_Instance.objects.get(session_task_id=first_session_task_no_repeat.session_task_id)
+        first_session_task_no_repeat_value = Session_Task_Instance_Value.objects.get(session_task_instance_id=first_session_task_no_repeat_instance.session_task_instance_id)
+        first_session_task_repeat = Session_Task.objects.get(session_id=first_session.session_id, task_id=self.task_repeat.task_id)
+        first_session_task_repeat_instance = Session_Task_Instance.objects.get(session_task_id=first_session_task_repeat.session_task_id)
+        first_session_task_repeat_value = Session_Task_Instance_Value.objects.get(session_task_instance_id=first_session_task_repeat_instance.session_task_instance_id)
+
+        second_session = views.generate_session(self.subject_uhn_bundle, self.session_type_web)
+        second_session_task_no_repeat = Session_Task.objects.get(session_id=second_session.session_id, task_id=self.task_no_repeat.task_id)
+        second_session_task_no_repeat_instance = Session_Task_Instance.objects.get(session_task_id=second_session_task_no_repeat.session_task_id)
+        second_session_task_no_repeat_value = Session_Task_Instance_Value.objects.get(session_task_instance_id=second_session_task_no_repeat_instance.session_task_instance_id)
+        second_session_task_repeat = Session_Task.objects.get(session_id=second_session.session_id, task_id=self.task_repeat.task_id)
+        second_session_task_repeat_instance = Session_Task_Instance.objects.get(session_task_id=second_session_task_repeat.session_task_id)
+        second_session_task_repeat_value = Session_Task_Instance_Value.objects.get(session_task_instance_id=second_session_task_repeat_instance.session_task_instance_id)
+
+        self.assertEqual(first_session_task_repeat_value.value, second_session_task_repeat_value.value)
+        self.assertNotEqual(first_session_task_no_repeat_value.value, second_session_task_no_repeat_value.value)
 
 class ViewsStartSessionTestCase(TestCase):
     '''
