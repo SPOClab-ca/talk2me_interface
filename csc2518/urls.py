6b34bc846f8640af020a441b0f76d400cedfb86f
Chloe Pou-Prom <chloepouprom@gmail.com>
2017-07-04 15:52:57 -0400

    Add API for retrieving active session

--- csc2518/urls.py
+++ csc2518/urls.py
@@ -44,6 +44,7 @@ urlpatterns = patterns('',
     # API for phone system
     url(r'^' + settings.SUBSITE_ID + 'phone/status$', 'datacollector.phone.status'),
     url(r'^' + settings.SUBSITE_ID + 'phone/login$', 'datacollector.phone.login'),
+    url(r'^' + settings.SUBSITE_ID + 'phone/active_session', 'datacollector.phone.active_session'),
     url(r'^' + settings.SUBSITE_ID + 'phone/session$', 'datacollector.phone.session'),
     url(r'^' + settings.SUBSITE_ID + 'phone/session/(?P<session_id>[0-9]+)$', 'datacollector.phone.session_id'),
     url(r'^' + settings.SUBSITE_ID + 'phone/session_task/(?P<session_task_id>[0-9]+)$', 'datacollector.phone.session_task'),
