c6c10a55b03bdbeff15b1c6c6c896af3326d71a4
Chloe Pou-Prom <chloepouprom@gmail.com>
2017-10-11 17:42:17 -0400

    Add views for other bundles

--- datacollector/adminui.py
+++ datacollector/adminui.py
@@ -11,35 +11,32 @@ from django.shortcuts import render_to_response
 from django.template import RequestContext
 
 from datacollector.models import (Bundle, Gender, Session, Session_Task, Session_Task_Instance,
-                                  Session_Task_Instance_Value, Session_Type, Settings, Subject, Subject_Bundle, 
+                                  Session_Task_Instance_Value, Session_Type, Settings, Subject, Subject_Bundle,
                                   Task, User)
 from datacollector.views import generate_session, delete_session
 
-from csc2518.settings import STATIC_URL
 from csc2518.settings import SUBSITE_ID
 
 # Set up mail authentication
-global email_username, email_name, website_hostname
-email_username = Settings.objects.get(setting_name="system_email").setting_value
-email_name = Settings.objects.get(setting_name="system_email_name").setting_value
-website_hostname = Settings.objects.get(setting_name="website_hostname").setting_value
-website_name = Settings.objects.get(setting_name="website_name").setting_value
+EMAIL_USERNAME = Settings.objects.get(setting_name="system_email").setting_value
+EMAIL_NAME = Settings.objects.get(setting_name="system_email_name").setting_value
+WEBSITE_HOSTNAME = Settings.objects.get(setting_name="website_hostname").setting_value
+WEBSITE_NAME = Settings.objects.get(setting_name="website_name").setting_value
 
 # Globals
-global global_passed_vars, date_format, age_limit, regex_email, regex_date, colour_lookup
-global_passed_vars = {
+GLOBAL_PASSED_VARS = {
     "website_id": "talk2me",
-    "website_name": website_name,
-    "website_email": email_username
+    "website_name": WEBSITE_NAME,
+    "website_email": EMAIL_USERNAME
 }
-website_root = '/'
-if SUBSITE_ID: website_root += SUBSITE_ID
+WEBSITE_ROOT = '/'
+if SUBSITE_ID:
+    WEBSITE_ROOT += SUBSITE_ID
 
-global DATA_ROW_SEP, DATA_COL_SEP, date_format, month_format, DAYS_PER_YEAR
 DATA_ROW_SEP = "#"
 DATA_COL_SEP = "|"
-date_format = "%Y-%m-%d"
-month_format = "%Y-%m"
+DATE_FORMAT = "%Y-%m-%d"
+MONTH_FORMAT = "%Y-%m"
 DAYS_PER_YEAR = 365.2425 # average length of year taking into account leap years
 
 UHN_NUM_SESSIONS = 7
@@ -101,7 +98,7 @@ def uhn_update_phone_pin(subject_id, phone_pin):
         Update phone_pin for subject
     '''
 
-    if len(str(phone_pin)):
+    if phone_pin:
         Subject.objects.filter(user_id=subject_id).update(phone_pin=phone_pin)
         return True
     else:
@@ -185,10 +182,10 @@ def uhn_session(request, bundle_uhn, user_id):
             'phone_pin_updated': phone_pin_updated,
             'username': User.objects.get(id=subject.user_id).username
         }
-        passed_vars.update(global_passed_vars)
+        passed_vars.update(GLOBAL_PASSED_VARS)
 
         return render_to_response('datacollector/uhn/adminui_sessions.html', passed_vars, context_instance=RequestContext(request))
-    return HttpResponseRedirect(website_root)
+    return HttpResponseRedirect(WEBSITE_ROOT)
 
 
 def uhn_dashboard(request, bundle_uhn):
@@ -227,7 +224,7 @@ def uhn_dashboard(request, bundle_uhn):
         is_sessions_created = []
         for bundle_subject in bundle_subjects:
             sessions_per_subject = Session.objects.filter(subject_id=bundle_subject.user_id)
-            if len(sessions_per_subject) > 0:
+            if sessions_per_subject:
                 is_sessions_created += [True]
             else:
                 is_sessions_created += [False]
@@ -248,10 +245,44 @@ def uhn_dashboard(request, bundle_uhn):
             'subjects': subjects
         }
 
-        passed_vars.update(global_passed_vars)
+        passed_vars.update(GLOBAL_PASSED_VARS)
         return render_to_response('datacollector/uhn/adminui.html', passed_vars, context_instance=RequestContext(request))
     else:
-        return HttpResponseRedirect(website_root)
+        return HttpResponseRedirect(WEBSITE_ROOT)
+
+def study_dashboard(request, study):
+    '''
+        Function for admin dashboard specific to a given study.
+    '''
+
+    is_authenticated = False
+    bundle = None
+    subject_bundle_users = []
+    subjects = []
+
+    if request.user.is_authenticated() and request.user.is_superuser:
+
+        bundle = Bundle.objects.get(name_id=('%s' % study))
+        is_authenticated = True
+
+        subject_bundle_users = Subject_Bundle.objects.filter(bundle=bundle)
+        bundle_subjects = [subject_bundle_user.subject for subject_bundle_user in subject_bundle_users]
+
+        for bundle_subject in bundle_subjects:
+            subjects.append({
+                'user_id': bundle_subject.user_id
+            })
+
+        passed_vars = {
+            'is_authenticated': is_authenticated,
+            'bundle': bundle,
+            'subjects': subjects,
+            'study': study
+        }
+
+        passed_vars.update(GLOBAL_PASSED_VARS)
+        return render_to_response('datacollector/adminui_study.html', passed_vars, context_instance=RequestContext(request))
+    return HttpResponseRedirect(WEBSITE_ROOT)
 
 def dashboard(request):
     '''
@@ -284,7 +315,8 @@ def dashboard(request):
             # - Number of users by gender (pie chart)
             piechart_gender = [DATA_COL_SEP.join(["Gender", "Users"])]
             piechart_gender += [DATA_COL_SEP.join([x.name, str(x.subject__count)]) for x in Gender.objects.annotate(Count('subject'))]
-            adminui_data += "<input class='adminui_data' type='hidden' chart-type='pie' data-title='Number of users by gender' value='" + DATA_ROW_SEP.join(piechart_gender) + "' />"
+            adminui_data += "<input class='adminui_data' type='hidden' chart-type='pie' data-title='Number of users by gender' value='" + \
+                            DATA_ROW_SEP.join(piechart_gender) + "' />"
 
             # - Number of users in different age brackets (bar graph). Bin the age in decades.
             bin_interval = 10
@@ -296,16 +328,19 @@ def dashboard(request):
             list_bins = list(age_bins)
             # Get the number of subjects in each bin, in the format e.g.: ["1-10"|"0", "11-20"|"0", "21-30"|"5", ...]
             bargraph_age = [DATA_COL_SEP.join(["Age", "Users"])]
-            bargraph_age += [DATA_COL_SEP.join([str(b) + "-" + str(b+bin_interval-1), str(age_data_binned.count(list_bins.index(b)+1))]) for b in list_bins]
-            adminui_data += "<input class='adminui_data' type='hidden' chart-type='bar' data-title='Number of users by age' value='" + DATA_ROW_SEP.join(bargraph_age) + "' />"
+            bargraph_age += [DATA_COL_SEP.join([str(b) + "-" + str(b+bin_interval-1), \
+                            str(age_data_binned.count(list_bins.index(b)+1))]) for b in list_bins]
+            adminui_data += "<input class='adminui_data' type='hidden' chart-type='bar' data-title='Number of users by age' value='" + \
+                            DATA_ROW_SEP.join(bargraph_age) + "' />"
 
             # - Number of tasks completed over time (by month since inception, where inception = first task completion date)
             truncate_date = connection.ops.date_trunc_sql('month', 'date_completed')
             completed_tasks = Session_Task.objects.filter(date_completed__isnull=False).extra({'month': truncate_date})
             tasks_by_month = completed_tasks.values('month').annotate(Count('session_task_id')).order_by('month')
             bargraph_tasks_by_month = [DATA_COL_SEP.join(["Month", "Tasks"])]
-            bargraph_tasks_by_month += [DATA_COL_SEP.join([x['month'].strftime(month_format), str(x['session_task_id__count'])]) for x in tasks_by_month]
-            adminui_data += "<input class='adminui_data' type='hidden' chart-type='bar' data-title='Number of tasks completed by month' value='" + DATA_ROW_SEP.join(bargraph_tasks_by_month) + "' />"
+            bargraph_tasks_by_month += [DATA_COL_SEP.join([x['month'].strftime(MONTH_FORMAT), str(x['session_task_id__count'])]) for x in tasks_by_month]
+            adminui_data += "<input class='adminui_data' type='hidden' chart-type='bar' data-title='Number of tasks completed by month' value='" + \
+                            DATA_ROW_SEP.join(bargraph_tasks_by_month) + "' />"
 
 
             # - Breakdown of each type of task that has been completed (task as IV and number of completions as DV) - pie chart
@@ -313,11 +348,13 @@ def dashboard(request):
             tasks_by_type = completed_tasks.values('task__name').annotate(Count('session_task_id')).order_by('task__name')
             piechart_tasks_by_type = [DATA_COL_SEP.join(["Task Type", "Tasks"])]
             piechart_tasks_by_type += [DATA_COL_SEP.join([x['task__name'], str(x['session_task_id__count'])]) for x in tasks_by_type]
-            adminui_data += "<input class='adminui_data' type='hidden' chart-type='bar' data-title='Number of tasks completed by type' value='" + DATA_ROW_SEP.join(piechart_tasks_by_type) + "' />"
+            adminui_data += "<input class='adminui_data' type='hidden' chart-type='bar' data-title='Number of tasks completed by type' value='" + \
+                            DATA_ROW_SEP.join(piechart_tasks_by_type) + "' />"
 
             # Show the avg number of completed samples per subject, for each task and overall
             num_samples_by_task_subject = Session_Task.objects.filter(date_completed__isnull=False).values('task', 'session__subject').annotate(Count('session_task_id'))
-            longitudinal_data = "<thead><tr><th>Task ID</th><th>Task Name</th><th>No. samples</th><th>No. subjects</th><th>Avg no. samples per subject per task</th></tr></thead><tbody>"
+            longitudinal_data = "<thead><tr><th>Task ID</th><th>Task Name</th><th>No. samples</th><th>No. subjects</th>" + \
+                                "<th>Avg no. samples per subject per task</th></tr></thead><tbody>"
             overall_avg_samples_per_subject = [0, 0]
             for task in Task.objects.filter(is_active=1).order_by('task_id'):
                 total_task_samples = sum([elem['session_task_id__count'] for elem in num_samples_by_task_subject if elem['task'] == task.task_id])
@@ -328,12 +365,15 @@ def dashboard(request):
                     avg_samples_per_subject = total_task_samples * 1.0 / total_task_subjects
                 else:
                     avg_samples_per_subject = 0
-                longitudinal_data += "<tr><td>" + str(task.task_id) + "</td><td>" + task.name + "</td><td>" + str(total_task_samples) + "</td><td>" + str(total_task_subjects) + "</td><td>" + "%.2f" % avg_samples_per_subject + "</td></tr>"
+                longitudinal_data += "<tr><td>" + str(task.task_id) + "</td><td>" + task.name + "</td><td>" + \
+                                     str(total_task_samples) + "</td><td>" + str(total_task_subjects) + "</td><td>" + \
+                                     "%.2f" % avg_samples_per_subject + "</td></tr>"
             if overall_avg_samples_per_subject[1] > 0:
                 overall_avg = overall_avg_samples_per_subject[0] * 1.0 / overall_avg_samples_per_subject[1]
             else:
                 overall_avg = 0
-            longitudinal_data += "<tr><td colspan='2'>TOTALS</td><td>" + str(overall_avg_samples_per_subject[0]) + "</td><td></td><td>" + "%.2f" % overall_avg + "</td></tr>"
+            longitudinal_data += "<tr><td colspan='2'>TOTALS</td><td>" + \
+                                 str(overall_avg_samples_per_subject[0]) + "</td><td></td><td>" + "%.2f" % overall_avg + "</td></tr>"
             longitudinal_data += "</tbody>"
 
             users_with_session = len(Session.objects.filter(end_date__isnull=False).values('subject').distinct())
@@ -360,7 +400,7 @@ def dashboard(request):
             'users_with_active_session': users_with_active_session,
             'users_with_longitudinal': users_with_longitudinal, 'bundles': bundles
         }
-        passed_vars.update(global_passed_vars)
+        passed_vars.update(GLOBAL_PASSED_VARS)
         return render_to_response('datacollector/adminui.html', passed_vars, context_instance=RequestContext(request))
     else:
-        return HttpResponseRedirect(website_root)
+        return HttpResponseRedirect(WEBSITE_ROOT)
