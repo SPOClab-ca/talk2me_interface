6b737d82a8c40a6e9f0425053d48109fffdf575b
Chloe Pou-Prom <chloepouprom@gmail.com>
2018-05-24 01:05:01 -0400

    Load custom filters in demographics template

--- datacollector/templatetags/dict_extras.py
+++ datacollector/templatetags/dict_extras.py
@@ -7,18 +7,18 @@ def hash(h, key):
     if key in h:
         return h[key]
     return ""
-    
+
 @register.filter
 def getlist(d, key):
     return d.getlist(key)
-    
+
 @register.filter
 def to_int(n):
     if type(n) is list:
         return [int(elem) for elem in n]
     else:
         return int(n)
-    
-@register.filter    
+
+@register.filter
 def str_concat(s1, s2):
     return str(s1) + str(s2)
